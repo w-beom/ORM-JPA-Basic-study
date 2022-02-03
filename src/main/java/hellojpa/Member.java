7e349ee36793e5b24c308874b91a07cd3f257808
w-beom <w_bum@naver.com>
2022-02-04 02:42:08 +0900

    데이터베이스 스키마 자동 생성
    - persistence.xml에 ddlauto 속성 설명 추가
    - Member 엔티티 테이블 제약조건 및 어노테이션 설명 추가

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,14 +1,17 @@
 package hellojpa;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-@Entity
+@Entity(name = "Member") // JPA가 Entity를 구분하는 속성 기본값은 클래스 이름
+//@Table(name = "MBR") // 데이터베이스에 MBR이라는 테이블과 매핑
+@Table(uniqueConstraints = {@UniqueConstraint(name = "NAME_AGE_UNIQUE", columnNames = {"NAME", "AGE"})}) // 유니크 제약 조건 추가
 public class Member {
-    
+
     @Id
     private Long id;
+    @Column(nullable = false, length = 10) // name값으 필수, 10자리 초과 X
     private String name;
+    private int age;
 
     public Long getId() {
         return id;
